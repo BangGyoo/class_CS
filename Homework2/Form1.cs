8cb48a6ac544be95221d9280bcd3701303bebc63
BangGyoo <BangGyoo12@gmail.com>
2019-03-24 15:21:05 +0900

    retry

---
+++ Homework2/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework2
+{
+    public partial class Form1 : Form
+    {
+        public partial class CustomForm : Form
+        {
+
+
+            static int FormCnt = 0;
+            static CustomForm[] forms;
+
+            public void CreateFormClicked(object sender, EventArgs e)
+            {
+                if (FormCnt == 1)
+                {
+                    FormCnt++;
+                    forms[1] = new CustomForm("두번째");
+                    forms[1].Show();
+                }
+                else if (FormCnt == 2)
+                {
+                    FormCnt++;
+                    forms[2] = new CustomForm("세번째");
+                    forms[2].Show();
+                }
+                else
+                {
+                    MessageBox.Show("마지막 줄의 버튼입니다.", "다음 줄 테스트");
+                }
+            }
+            public void DeleteFormClicked(object sender, EventArgs e)
+            {
+                if (FormCnt <= 1) MessageBox.Show("모두 지웠습니다.", "그만지워");
+                else
+                {
+                    forms[FormCnt-1].Close();
+                }
+            }
+            
+
+            public CustomForm()
+            {
+                FormCnt++;
+
+                forms = new CustomForm[3];
+                forms[0] = this;
+                
+                
+
+                this.Text = "첫번째 폼";
+                MenuStrip mainMenu = new MenuStrip();
+                ToolStripMenuItem open = new ToolStripMenuItem();
+                open.Text = "폼 생성";
+                ToolStripMenuItem nextOpen = new ToolStripMenuItem();
+                nextOpen.Text = "두번째 폼 열기";
+                ToolStripMenuItem close = new ToolStripMenuItem();
+                close.Text = "폼 제거";
+
+                nextOpen.Click += new EventHandler(CreateFormClicked);
+                close.Click += new EventHandler(DeleteFormClicked);
+                open.DropDownItems.Add(nextOpen);
+                mainMenu.Items.Add(open);
+                mainMenu.Items.Add(close);
+
+                Controls.Add(mainMenu);
+                
+            }
+            public CustomForm(String title)
+            {
+                this.Text = title + " 폼";
+                Button btn = new Button();
+                btn.Text = title + " 바";
+                btn.Location = new Point(100, 50);
+                btn.Click += new EventHandler(CreateFormClicked);
+                Controls.Add(btn);
+                FormClosing += new FormClosingEventHandler(FormClosedEventHandler);
+            }
+            void FormClosedEventHandler(object sender,EventArgs e)
+            {
+                FormCnt--;
+            }
+
+        }
+
+
+        public Form1()
+        {
+           
+            InitializeComponent();
+            IsMdiContainer = true;
+            CustomForm form = new CustomForm();
+            form.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+    }
+}
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+
+        private int btn2_loop = 1;
+        private int btn3_loop = 1;
+        private const int MARGIN = 5;
+        private int elapsedTime = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            //MessageBox.Show("버튼들을 지울까여?");
+            FormClosing += new FormClosingEventHandler(Form_FormClosing);
+        }    
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            elapsedTime++;
+            //if (elapsedTime == 4)
+            // {
+            //     if (MessageBox.Show("470초 경과 했습니다.", Text,
+            //    MessageBoxButtons.YesNo, MessageBoxIcon.Question,
+            //    MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            //     {
+            //         Application.Exit();
+            //     }
+            // }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Button btn = new Button();
+                btn.Text = "동적생성" + i;
+                btn.Text += "번째";
+                btn.Width = 100;
+                btn.Height = 20;
+                Controls.Add(btn);
+                btn.Location = new Point(15, 50 + i * (20 + MARGIN));
+                btn.Visible = true;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button btn = new Button();
+            btn.Text = "동적생성" + btn2_loop;
+            btn.Text += "번째";
+            btn.Width = 100;
+            btn.Height = 20;
+
+            Controls.Add(btn);
+            btn.Location = new Point(150, 50 + btn2_loop * (20 + MARGIN));
+            btn.Visible = true;
+            btn2_loop++;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button btn = new Button();
+            btn.Text = "동적생성" + btn3_loop;
+            btn.Text += "번째";
+            btn.Width = 100;
+            btn.Height = 20;
+
+            Controls.Add(btn);
+            btn.Location = new Point(300, 50 + btn3_loop * (20 + MARGIN));
+            btn.Visible = true;
+            btn.Click += new EventHandler(button3_Click);
+            btn3_loop++;
+        }
+
+        private void Form_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("버튼 삭제 할까요?", Text,
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button1) == DialogResult.No)
+            {
+                e.Cancel = true;
+
+            }
+
+            else
+            {
+                Controls.Clear();
+                MessageBox.Show((int)(elapsedTime * 0.1) + "초 경과 했습니다.");
+               
+                e.Cancel = false;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
