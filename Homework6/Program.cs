78656f11e0da88af4134cdd32f8dcc2ca671eded
BangGyoo <BangGyoo12@gmail.com>
2019-05-10 12:11:14 +0900

    Success homework6

---
+++ Homework6/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework6
+{
+    public class Moniter
+    {
+        public IBorder border;
+        public Moniter(IBorder border)
+        {
+            this.border = border;
+        }
+        public void start()
+        {
+            border.WriteBorder();
+        }
+    }
+
+    public interface IBorder
+    {
+        void WriteBorder();
+    }
+    public class Star : IBorder
+    {
+        public void WriteBorder()
+        {
+            Console.WriteLine("**************");
+            Console.WriteLine("*Hello World!*");
+            Console.WriteLine("**************");
+        }
+    }
+    public class Slash : IBorder
+    {
+        public void WriteBorder()
+        {
+            Console.WriteLine("//////////////");
+            Console.WriteLine("/Hello World!/");
+            Console.WriteLine("//////////////");
+        }
+    }
+    public class Wave : IBorder
+    {
+        public void WriteBorder()
+        {
+            Console.WriteLine("~~~~~~~~~~~~~~");
+            Console.WriteLine("~Hello World!~");
+            Console.WriteLine("~~~~~~~~~~~~~~");
+        }
+    }
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wave wave = new Wave();
+            Moniter hello1 = new Moniter(wave);
+            hello1.start();
+            Moniter hello2 = new Moniter(new Star());
+            hello2.start();
+            Moniter hello3 = new Moniter(new Slash());
+            hello3.start();
+
+        }
+    }
+}
