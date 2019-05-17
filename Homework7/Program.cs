33a61c358bfe5bed7429af0e441a725d45c94283
BangGyoo <BangGyoo12@gmail.com>
2019-05-17 12:39:22 +0900

    upload homework ver 7

---
+++ Homework7/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7
+{
+    abstract class AbstractDisplay
+    {
+        protected virtual void Print() { }
+        protected virtual void Open() { }
+        protected virtual void Close() { }
+        
+        public void Write(char ch)
+        {
+            for(int i=0; i<5; i++)
+                Console.Write(ch);
+        }
+        public void Write(string str)
+        {
+            Console.Write(str);
+        }
+        public void Display()     // 탬플릿 메소드 패턴 
+        {
+            Open();
+            Print();
+            Close();
+            
+        }
+
+    }
+    class CharDisplay : AbstractDisplay
+    {
+        private char ch;
+        public CharDisplay(char ch)
+        {
+            this.ch = ch;
+        }
+        protected override void Open() { Write("<<"); }
+        protected override void Print() { Write(ch); }
+        protected new void Close() { base.Close(); Write(">>"); }
+
+    }
+    class StringDisplay : AbstractDisplay
+    {
+        private string str;
+        private int width;
+        public StringDisplay(string str)
+        {
+            this.str = str;
+            this.width = str.Length;
+        }
+        protected override void Open() { PrintLine(); }
+        protected override void Print() { Write("|" + str + "|"); }
+        protected override void Close() { PrintLine();  }
+        void PrintLine()
+        {
+            Write("+");
+            for(int i = 0; i < width; i++)
+            {
+                Write("-");
+            }
+            Write("+");
+            
+        }
+        
+    }
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            AbstractDisplay d1 = new CharDisplay('H');
+            AbstractDisplay d2 = new StringDisplay("Hello, world!");
+            d1.Display();
+            d2.Display();
+        }
+    }
+}
