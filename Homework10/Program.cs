9c8be85f374e0f1d2514272281ca654cd42f7e74
BangGyoo <BangGyoo12@gmail.com>
2019-05-31 12:51:43 +0900

    homework10 ..ing

---
+++ Homework10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+//using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Homework10
+//{
+//    delegate void MyDelegate(int a);
+
+//    class Market
+//    {
+//        public event MyDelegate CustomerEvent;
+
+//        public void BuySomething(int CustomerNo)
+//        {
+//            if (CustomerNo == 30)
+//                CustomerEvent(CustomerNo);
+//        }
+//    }
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+//            Market market = new Market();
+//            market.CustomerEvent += new MyDelegate();
+
+//            for (int customerNo = 0; customerNo < 100; customerNo += 10)
+//                market.BuySomething(customerNo);
+//        }
+//    }
+
+//
+
+namespace Homework10
+{
+    public class EventForm :Form
+    {
+
+    }
+}
+
