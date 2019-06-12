32dc95fc2665a3394c5f3c5a27c6ff1fcf21af6b
BangGyoo <BangGyoo12@gmail.com>
2019-06-12 16:05:10 +0900

    ..ing

---
+++ proj3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proj3
+{
+    abstract class AbstractBase
+    {
+        protected void PrivateMethodA()
+        {
+            Console.WriteLine("AbstractBase.PrivateMethod()");
+        }
+        public void PublicMethodA()
+        {
+            Console.WriteLine("AbstractBase.PublicMethod()");
+        }
+        public abstract void AbstractMethodA();
+    }
+    class Derived : AbstractBase
+    {
+
+        public override void AbstractMethodA()
+        {
+            Console.WriteLine("Derived.AbstractMethodA()");
+            PrivateMethodA();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractBase obj = new Derived();
+            obj.AbstractMethodA();
+            obj.PublicMethodA();
+        }
+    }
+}
